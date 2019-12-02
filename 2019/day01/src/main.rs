1c017eade5f95b95a413f9b924f3840ceddae2f5
Dathan Bennett <dathanb@netflix.com>
2019-12-01 22:53:10 -0700

    2019 day 2 part 2 done

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -2,6 +2,14 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
+    part1();
+
+    part2();
+
+    Ok(())
+}
+
+fn part1() -> io::Result<()> {
     let file = File::open("resources/input.txt").unwrap();
 
     let reader = BufReader::new(file);
@@ -13,6 +21,31 @@ fn main() -> io::Result<()> {
         let num: i32 = line.parse().unwrap();
         let weight = (num /3)-2;
         total += weight;
+    }
+
+    println!("{}", total);
+
+    Ok(())
+}
+
+fn part2() -> io::Result<()> {
+    let file = File::open("resources/input.txt").unwrap();
+
+    let reader = BufReader::new(file);
+
+    let mut total: i32 = 0;
+
+    for line_result in reader.lines() {
+        let line = line_result.expect("Error getting next line from file.");
+        let num: i32 = line.parse().unwrap();
+        let mut delta_fuel_weight = (num / 3) - 2;
+        while delta_fuel_weight > 0 {
+            total += delta_fuel_weight;
+            delta_fuel_weight = (delta_fuel_weight / 3) - 2;
+            if delta_fuel_weight < 0 {
+                delta_fuel_weight = 0
+            }
+        }
     }
 
     println!("{}", total);
