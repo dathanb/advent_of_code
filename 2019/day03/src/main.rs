5a259efdfe7e9ec56491b2e9eb039499201b6b0c
Dathan Bennett <dathanb@netflix.com>
2019-12-04 12:19:22 -0800

    2019 Day 3 part 2 solution

--- 2019/day03/src/main.rs
+++ 2019/day03/src/main.rs
@@ -23,20 +23,30 @@ fn part1(lines: &Vec<&str>) {
     let first_wire_points = get_point_set(lines[0]);
     let second_wire_points = get_point_set(lines[1]);
 
-    let first_wire_coordinates:HashSet<&Coordinate> = first_wire_points.keys().collect();
-    let second_wire_coordinates:HashSet<&Coordinate> = second_wire_points.keys().collect();
+    let first_wire_coordinates: HashSet<&Coordinate> = first_wire_points.keys().collect();
+    let second_wire_coordinates: HashSet<&Coordinate> = second_wire_points.keys().collect();
     let intersection_points = first_wire_coordinates.intersection(&second_wire_coordinates);
 
-    println!("{}", intersection_points.map(|c| Coordinate{x:0, y:0}.distance(**c)).min().unwrap());
+    println!("{}", intersection_points.map(|c| Coordinate { x: 0, y: 0 }.distance(**c)).min().unwrap());
 }
 
 fn part2(lines: &Vec<&str>) {
-    let _set1 = get_point_set(lines[0]);
-    let _set2 = get_point_set(lines[1]);
+    let first_wire = get_point_set(lines[0]);
+    let second_wire = get_point_set(lines[1]);
+
+    let mut min_distance: i64 = std::i32::MAX.into();
+    for coordinate in first_wire.keys() {
+        let distance = first_wire.get(coordinate)
+            .map_or(std::i32::MAX.into(), |d| d + second_wire.get(coordinate).unwrap_or(&std::i32::MAX.into()));
+        if distance < min_distance {
+            min_distance = distance;
+        }
+    }
+    println!("{}", min_distance);
 }
 
-fn get_point_set(line: &str) -> HashMap<Coordinate, i32> {
-    let mut all_points: HashMap<Coordinate, i32> = HashMap::new();
+fn get_point_set(line: &str) -> HashMap<Coordinate, i64> {
+    let mut all_points: HashMap<Coordinate, i64> = HashMap::new();
     let movements: Vec<Movement> = line.split(",")
         .map(|n| n.trim())
         .map(|d| Movement::parse(d))
@@ -57,8 +67,8 @@ fn get_point_set(line: &str) -> HashMap<Coordinate, i32> {
 
 #[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
 struct Coordinate {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 impl Coordinate {
@@ -72,19 +82,19 @@ impl Coordinate {
         }
     }
 
-    fn distance(&self, other: Coordinate) -> i32 {
+    fn distance(&self, other: Coordinate) -> i64 {
         (self.x - other.x).abs() + (self.y - other.y).abs()
     }
 }
 
 struct Movement<'a> {
     direction: &'a str,
-    distance: i32,
+    distance: i64,
 }
 
 impl<'a> Movement<'a> {
     fn parse(text: &str) -> Movement {
-        let mut movement: Movement = Movement {direction: "", distance: 0};
+        let mut movement: Movement = Movement { direction: "", distance: 0 };
         match text.chars().nth(0) {
             Some('U') => movement.direction = "U",
             Some('D') => movement.direction = "D",
@@ -93,7 +103,7 @@ impl<'a> Movement<'a> {
             _ => panic!("Unrecognized direction in string {}", text),
         }
 
-        movement.distance = text[1..text.len()].parse::<i32>().unwrap();
+        movement.distance = text[1..text.len()].parse::<i64>().unwrap();
 
         movement
     }
