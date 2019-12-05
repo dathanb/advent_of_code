97e79c99ee146277d6380de340767b2f621fda7b
Dathan Bennett <dathanb@netflix.com>
2019-12-04 22:19:47 -0800

    2019 day 4 part 2

--- 2019/day04/src/main.rs
+++ 2019/day04/src/main.rs
@@ -2,23 +2,22 @@ use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("resources/input.txt").unwrap();
-
-    part1(&contents);
-
-    println!("Hello, world!");
-}
-
-fn part1(input: &str) {
-    let parts: Vec<i32> = input.split("-")
+    let parts: Vec<i32> = contents.split("-")
         .map(|s| s.parse::<i32>().unwrap())
         .collect();
 
     let lower_bound = parts[0];
     let upper_bound = parts[1];
 
+    part1(lower_bound, upper_bound);
+
+    part2(lower_bound, upper_bound);
+}
+
+fn part1(lower_bound: i32, upper_bound: i32) {
     let mut count = 0;
 
-    for n in lower_bound .. upper_bound+1 {
+    for n in lower_bound..upper_bound + 1 {
         if has_six_digits(n) && has_repeat(n) && is_non_decreasing(n) {
             count += 1;
         }
@@ -27,6 +26,18 @@ fn part1(input: &str) {
     println!("{}", count);
 }
 
+fn part2(lower_bound: i32, upper_bound: i32) {
+    let mut count = 0;
+    for n in lower_bound..upper_bound + 1 {
+        if has_six_digits(n) && has_two_digit_only_repeat(n) && is_non_decreasing(n) {
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
+}
+
+
 fn has_six_digits(n: i32) -> bool {
     n >= 100000 && n <= 999999
 }
@@ -34,18 +45,39 @@ fn has_six_digits(n: i32) -> bool {
 fn has_repeat(n: i32) -> bool {
     let digits: Vec<i32> = get_digits(n);
     let mut repeat = false;
-    for i in 0..digits.len()-1 {
-        if digits[i] == digits[i+1] {
+    for i in 0..digits.len() - 1 {
+        if digits[i] == digits[i + 1] {
             repeat = true;
         }
     }
     repeat
 }
 
+fn has_two_digit_only_repeat(n: i32) -> bool {
+    let digits = get_digits(n);
+    let mut repeat = 1;
+    let mut has_two_digit_repeat = false;
+    for i in 0..digits.len() - 1 {
+        if digits[i] == digits[i + 1] {
+            repeat += 1;
+        } else {
+            if repeat == 2 {
+                has_two_digit_repeat = true;
+            }
+            repeat = 1;
+        }
+    }
+    if repeat == 2 {
+        has_two_digit_repeat = true;
+    }
+
+    has_two_digit_repeat
+}
+
 fn is_non_decreasing(n: i32) -> bool {
     let digits: Vec<i32> = get_digits(n);
-    for i in 0..digits.len()-1 {
-        if digits[i+1] < digits[i] {
+    for i in 0..digits.len() - 1 {
+        if digits[i + 1] < digits[i] {
             return false;
         }
     }
