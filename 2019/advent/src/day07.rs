4169b515234535564e6305906a5d7256041a315b
Dathan Bennett <dathanb@netflix.com>
2019-12-13 17:51:25 -0800

    Add part2 tests (still red)

--- 2019/advent/src/day07.rs
+++ 2019/advent/src/day07.rs
@@ -137,4 +137,22 @@ mod tests {
         let actual_output = part1().unwrap();
         assert_eq!(actual_output, "65464");
     }
+
+    #[test]
+    fn test_part2_input1() {
+        let input = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
+        let expected_output = "139629729";
+        let computer = computer::parse(input);
+        let actual_output = compute_part2(&computer).unwrap();
+        assert_eq!(actual_output, expected_output);
+    }
+
+    #[test]
+    fn test_part2_input2() {
+        let input = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10";
+        let expected_output = "18216";
+        let computer = computer::parse(input);
+        let actual_output = compute_part2(&computer).unwrap();
+        assert_eq!(actual_output, expected_output);
+    }
 }
