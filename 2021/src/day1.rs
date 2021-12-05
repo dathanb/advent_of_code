ad991a1a6110500fdf6a58a3e294efb73b69ec9a
Dathan Bennett <dathan@gmail.com>
2021-12-04 22:08:06 -0800

    re-implement read_lines and add read_depths

--- 2021/src/day1.rs
+++ 2021/src/day1.rs
@@ -3,11 +3,25 @@ pub mod day1 {
 	use std::fs::File;
 	use std::io::{self, BufRead};
 
-	pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    /// Returns the lines from a file;
+    ///
+    /// This isn't day1-specific and should be moved to a shared library when we start working on a
+    /// new day.
+	pub fn read_lines<P>(filename: P) -> Result<Vec<String>, io::Error>
 	where P: AsRef<std::path::Path>, {
 		let file = File::open(filename)?;
-		Ok(io::BufReader::new(file).lines())
+        let reader = io::BufReader::new(file);
+        reader.lines().collect::<Result<_, _>>()
 	}
+
+    /// Reads the lines from a file containing a single integer per line and returns them as a
+    /// collection of integers
+    pub fn read_depths<P>(filename: P) -> Result<Vec<i32>, io::Error>
+    where P: AsRef<std::path::Path>, {
+        let lines = read_lines(filename)?;
+        Ok(lines.iter().map(|s| s.parse::<i32>().unwrap()).collect())
+    }
+
 
 //    pub fn parse_lines() -> io::Result<io::Lines<io::BufReader<std::fs::File>>> {
 //	}
--- 2021/src/day1_tests.rs
+++ 2021/src/day1_tests.rs
@@ -1,13 +1,20 @@
 #[cfg(test)]
 mod day1_tests {
 	use crate::day1::day1::read_lines;
+	use crate::day1::day1::read_depths;
 
     #[test]
-    pub fn it_works() {
-		println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
-        if let Ok(_lines) = read_lines("./src/day1_input.txt") {
-		} else {
-			panic!("Failed to read lines");
-		}
+    pub fn read_lines_works() {
+        let lines = read_lines("./src/read_lines_test_input.txt").unwrap();
+        assert_eq!("500", lines[0]);
+        assert_eq!("1000", lines[1]);
     }
+
+    #[test]
+    pub fn read_depths_works() {
+        let depths = read_depths("./src/read_lines_test_input.txt").unwrap();
+        assert_eq!(500, depths[0]);
+        assert_eq!(1000, depths[1]);
+    }
+
 }
