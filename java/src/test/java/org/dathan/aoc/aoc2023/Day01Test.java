072c8fc6f5e486827de70b47afb4b447be7a03ba
Dathan Bennett <dathan@gmail.com>
2023-12-01 19:15:56 -0800

    Add 2023 day 1 to the main java project

--- java/src/test/java/org/dathan/aoc/aoc2023/Day01Test.java
+++ java/src/test/java/org/dathan/aoc/aoc2023/Day01Test.java
@@ -9,35 +9,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day01Test {
     @Test
     public void part1TestData() {
-        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/day01-part1-test.txt"));
+        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/2023/day01-part1-test.txt"));
 
         assertEquals(142, new Day01().part1(input));
     }
 
     @Test
     public void part1() {
-        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/day01.txt"));
+        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/2023/day01.txt"));
 
         assertEquals(54667, new Day01().part1(input));
     }
 
     @Test
     public void part2TestData() {
-        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/day01-part2-test.txt"));
+        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/2023/day01-part2-test.txt"));
 
         assertEquals(281, new Day01().part2(input));
     }
 
     @Test
     public void part2ExtraTestData() {
-        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/day01-part2-extratest.txt"));
+        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/2023/day01-part2-extratest.txt"));
 
         assertEquals(312, new Day01().part2(input));
     }
 
     @Test
     public void part2() {
-        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/day01.txt"));
+        List<String> input = IOUtil.getLines(IOUtil.readFromResource("/2023/day01.txt"));
 
         assertEquals(54203, new Day01().part2(input));
     }
